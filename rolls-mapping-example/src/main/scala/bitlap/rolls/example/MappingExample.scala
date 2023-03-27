bca04532243369b308f72b86034594c5eeab0a11
梦境迷离 <dreamylost@outlook.com>
2023-03-27 18:09:48 +0800

    rename

--- rolls-mapping-example/src/main/scala/bitlap/rolls/example/MappingExample.scala
+++ rolls-mapping-example/src/main/scala/bitlap/rolls/example/MappingExample.scala
@@ -8,13 +8,13 @@ import scala.concurrent.Future
  */
 object MappingExample extends App:
 
-  // If not found, continue using `rhs`, otherwise use mapping by sql.
-  @RhsMapping val re1 = "menu.operate" // ast: mods val name: tpt = rhs
-
-  @CustomRhsMapping(idColumn = "id", nameColumns = "resource.action", tableName = "schema.table") val re2 =
-    "menu.operate"
-
-  println(re1) // `select id from schema.table where resource = 'menu' and action = 'operate'`
-  println(re2) // `select id from schema.table where resource = 'menu' and action = 'operate'`
+//  // If not found, continue using `rhs`, otherwise use mapping by sql.
+//  @RhsMapping val re1 = "menu.operate" // ast: mods val name: tpt = rhs
+//
+//  @CustomRhsMapping(idColumn = "id", nameColumns = "resource.action", tableName = "schema.table") val re2 =
+//    "menu.operate"
+//
+//  println(re1) // `select id from schema.table where resource = 'menu' and action = 'operate'`
+//  println(re2) // `select id from schema.table where resource = 'menu' and action = 'operate'`
 
 end MappingExample
